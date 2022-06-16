0e7bb622913289037a759da7bd8178f2449d6c1b
n9k <originalcocaineninja@protonmail.com>
2022-06-16 03:14:46 +0000

    anonstream/__main__.py: tidy magic numbers

--- anonstream/__main__.py
+++ anonstream/__main__.py
@@ -5,6 +5,22 @@ import uvicorn
 
 from anonstream import create_app
 
+DEFAULT_PORT = 5051
+DEFAULT_CONFIG = 'config.toml'
+
+def want_rel(path):
+    '''
+    Prepend './' to relative paths.
+    >>> want_rel('/some/abs/path')
+    '/some/abs/path'
+    >>> want_rel('config.toml')
+    './config.toml'
+    '''
+    if os.path.isabs(path):
+        return path
+    else:
+        return os.path.join('.', path)
+
 formatter = lambda prog: argparse.HelpFormatter(prog, max_help_position=26)
 parser = argparse.ArgumentParser(
     'python -m anonstream',
@@ -17,14 +33,14 @@ parser.add_argument(
     default=os.environ.get('ANONSTREAM_CONFIG', 'config.toml'),
     help=(
         'location of config.toml '
-        '(default: $ANONSTREAM_CONFIG or ./config.toml)'
+        f'(default: $ANONSTREAM_CONFIG or {want_rel(DEFAULT_CONFIG)})'
     ),
 )
 parser.add_argument(
     '--port', '-p',
     type=int,
-    default=5051,
-    help='bind webserver to this port (default: 5051)',
+    default=DEFAULT_PORT,
+    help=f'bind webserver to this port (default: {DEFAULT_PORT})',
 )
 args = parser.parse_args()
 
