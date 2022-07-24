bab82df319885202ba83f12695b23b8d22c097df
n9k <originalcocaineninja@protonmail.com>
2022-07-24 10:20:00 +0000

    Emotes: allow automatic width/height

--- anonstream/helpers/chat.py
+++ anonstream/helpers/chat.py
@@ -23,10 +23,12 @@ def get_scrollback(messages):
 @lru_cache
 def get_emote_markup(emote_name, emote_file, emote_width, emote_height):
     emote_name_markup = escape(emote_name)
+    width = '' if emote_width is None else f'width="{escape(emote_width)}" '
+    height = '' if emote_height is None else f'height="{escape(emote_height)}" '
     return Markup(
         f'''<img class="emote" '''
         f'''src="{escape(url_for('static', filename=emote_file))}" '''
-        f'''width="{escape(emote_width)}" height="{escape(emote_height)}" '''
+        f'''{width}{height}'''
         f'''alt="{emote_name_markup}" title="{emote_name_markup}">'''
     )
 
