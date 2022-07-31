c2094f1d89ae249964698bee282166fa8c448cf4
n9k <originalcocaineninja@protonmail.com>
2022-07-31 22:56:34 +0000

    Emotes: reorganize

--- anonstream/helpers/chat.py
+++ anonstream/helpers/chat.py
@@ -2,13 +2,10 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import hashlib
-from functools import lru_cache
 
-import markupsafe
-from quart import current_app, escape, url_for, Markup
+from quart import current_app
 
 CONFIG = current_app.config
-EMOTES = current_app.emotes
 
 def generate_nonce_hash(nonce):
     parts = CONFIG['SECRET_KEY'] + b'nonce-hash\0' + nonce.encode()
@@ -19,24 +16,3 @@ def get_scrollback(messages):
     if len(messages) < n:
         return messages
     return list(messages)[-n:]
-
-@lru_cache
-def get_emote_markup(emote_name, emote_file, emote_width, emote_height):
-    emote_name_markup = escape(emote_name)
-    width = '' if emote_width is None else f'width="{escape(emote_width)}" '
-    height = '' if emote_height is None else f'height="{escape(emote_height)}" '
-    return Markup(
-        f'''<img class="emote" '''
-        f'''src="{escape(url_for('static', filename=emote_file))}" '''
-        f'''{width}{height}'''
-        f'''alt="{emote_name_markup}" title="{emote_name_markup}">'''
-    )
-
-def insert_emotes(markup):
-    assert isinstance(markup, markupsafe.Markup)
-    for emote in EMOTES:
-        emote_markup = get_emote_markup(
-            emote['name'], emote['file'], emote['width'], emote['height'],
-        )
-        markup = emote['regex'].sub(emote_markup, markup)
-    return Markup(markup)
